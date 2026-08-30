6f21c2f0b240147996ab9e14389c5a56f4428d0c
cy.ryu <tou7262@gmail.com>
2026-08-31 01:22:53 +0900

    chore: initialize CopyCodeReference VSIX project

---
+++ source.extension.cs
@@ -0,0 +1,13 @@
+namespace CopyCodeReference
+{
+    internal sealed partial class Vsix
+    {
+        public const string Id = "CopyCodeReference.8ac855e5-611b-4f49-b75f-8519d132f8b6";
+        public const string Name = "Copy Code Reference";
+        public const string Description = @"Copy selected Visual Studio code with its file path and line numbers.";
+        public const string Language = "en-US";
+        public const string Version = "0.1.0";
+        public const string Author = "cy.ryu";
+        public const string Tags = "copy, clipboard, editor, selection, file path, line number, reference";
+    }
+}
