135af5bd1bb440fe0674044a82c9af9ed9452cb2
cy.ryu <tou7262@gmail.com>
2026-08-31 01:22:53 +0900

    feat: add Copy Code Reference command

---
+++ VSCommandTable.cs
@@ -0,0 +1,16 @@
+namespace CopyCodeReference
+{
+    using System;
+
+    internal sealed partial class PackageGuids
+    {
+        public const string CopyCodeReferenceString = "cf0867e8-3f40-427f-9f65-d4da570b0145";
+        public static Guid CopyCodeReference = new Guid(CopyCodeReferenceString);
+    }
+
+    internal sealed partial class PackageIds
+    {
+        public const int CopyCodeReferenceGroup = 0x0001;
+        public const int CopyCodeReferenceCommand = 0x0100;
+    }
+}
